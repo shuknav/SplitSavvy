284b2f82a04979cb8a042661406961478ab9929d
ChilliSauce <navshukla30@gmail.com>
2025-07-10 23:31:54 +0530

    prepare for validity success page login

--- app/src/pages/success.jsx
+++ app/src/pages/success.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import img_building from "../assets/img_building.png";
 
 function Success() {
-  // temporary maintenance page
+  const navigate = useNavigate();
+  // temporary maintenance and dashboard page
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
       <h1 className="text-4xl md:text-5xl font-bold mb-6">
