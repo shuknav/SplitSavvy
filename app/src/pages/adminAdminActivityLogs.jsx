23d78af63fcd158a8580912f0c660d43c0f203ab
ChilliSauce <navshukla30@gmail.com>
2025-07-11 20:34:53 +0530

    feat: add admin activity logs non functional

---
+++ app/src/pages/adminAdminActivityLogs.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function AdminActivityLog() {
+  return (
+    <>
+      <h2 className="text-xl font-bold mb-4 text-white">Admin Activity Logs</h2>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Action</TableCell>
+              <TableCell>admin id</TableCell>
+              <TableCell>on date and time</TableCell>
+              <TableCell>ip</TableCell>
+            </TableRow>
+          </TableHead>
+          {/* placeholder dummy data */}
+          <TableBody>
+            <TableRow>
+              <TableCell>password change</TableCell>
+              <TableCell>1</TableCell>
+              <TableCell>01/01/25 20:09:00</TableCell>
+              <TableCell>some ip address</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
+
+export default AdminActivityLog;
