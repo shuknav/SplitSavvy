373dc5dfd26b80fa1743d64665ae4d29aa5dad5b
ChilliSauce <navshukla30@gmail.com>
2025-07-15 18:30:00 +0530

    feat: add temporary mock-based admin login for initial UI verification

---
+++ app/src/pages/landing/AdminLanding.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import InputField from "../../components/InputField";
+import ButtonField from "../../components/ButtonField";
+import { AdminLogin } from "../../api/admin";
+
+function AdminLanding() {
+  const [inputData, setinputData] = useState({
+    username: "",
+    password: "",
+  });
+
+  function HandleInputChange(field, value) {
+    //function handles input fields change
+    setinputData((prev) => ({ ...prev, [field]: value }));
+  }
+
+  async function HandleClick() {
+    if (inputData.username === "whisker") {
+      if (inputData.password === "password") {
+        console.log("welcome");
+      } else {
+        console.log("wrong password try again");
+      }
+    } else {
+      console.log("not a valid admin");
+    }
+  }
+
+  return (
+    <>
+      <main className="flex flex-col items-center justify-center flex-grow px-6 py-16 text-center text-white">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4">Admin Login</h2>
+        <Box sx={{ width: "100%", maxWidth: 400 }} className="mb-6">
+          <InputField
+            label="Username"
+            id="admin-id"
+            type="text"
+            value={inputData.username}
+            handleChange={(val) => {
+              HandleInputChange("username", val);
+            }}
+          />
+          <InputField
+            label="Password"
+            id="admin-password"
+            type="password"
+            value={inputData.password}
+            handleChange={(val) => {
+              HandleInputChange("password", val);
+            }}
+          />
+        </Box>
+        <ButtonField text="Continue" handleClick={HandleClick} />
+      </main>
+    </>
+  );
+}
+
+export default AdminLanding;
