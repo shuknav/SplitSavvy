385acd733ffa7c68c6921744e094e1ee99f1e329
ChilliSauce <navshukla30@gmail.com>
2025-07-23 13:00:41 +0530

    chore: removed unused import from Settings.jsx

--- app/src/pages/admin/settings/Settings.jsx
+++ app/src/pages/admin/settings/Settings.jsx
@@ -4,7 +4,6 @@ import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
-import Tooltip from "@mui/material/Tooltip";
 import PasswordChange from "./PasswordChange";
 import CreateAdmin from "./CreateAdmin";
 import SuperUserPermissions from "./SuperUserPermissions";
