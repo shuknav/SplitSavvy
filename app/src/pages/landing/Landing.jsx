121b2cc55898dec8ff53c64c9466f6d5202727ae
ChilliSauce <navshukla30@gmail.com>
2025-07-31 18:06:21 +0530

    Refactor: removed not used imports

--- app/src/pages/landing/Landing.jsx
+++ app/src/pages/landing/Landing.jsx
@@ -4,10 +4,7 @@ import Footer from "../../components/Footer";
 import Hero from "../../sections/home/Hero";
 import Features from "../../sections/home/Features";
 import JoinWaitlist from "../../sections/home/JoinWaitlist";
-import LandingHero from "../../sections/home/landingPageHero";
-import KeyFeatures from "../../sections/home/landingPageKeyFeatures";
 import AboutColiving from "../../sections/home/AboutColiving";
-import Waitlist from "../../sections/home/landingPageWaitlist";
 
 function Landing() {
   //structure of lading page made up of many different components
